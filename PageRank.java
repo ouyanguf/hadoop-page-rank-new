72c51eeae453e24afa6fa376661902d775e727c1
Yang Ou <getouyang@gmail.com>
2014-01-28 20:15:21 -0500

    update rank iterations

--- PageRank.java
+++ PageRank.java
@@ -14,11 +14,14 @@ public class PageRank extends Configured implements Tool {
 	
 	public int run(String[] args) throws Exception {
                 
-                Path inpath = new Path(args[0]);
-                Path outpath = new Path(args[1]);
+                Path inPath = new Path(args[0]);
+                Path outPath = new Path(args[1]);
+                Path graphPath = new Path(args[1]+"-Graph-Info");
+	        Path countPath = new Path(args[1] + "-Count-N"); 
+	        Path iterInPath;// = new Path(" ");
+	        Path iterOutPath;// = new Path(" ");
                 
                 JobConf conf = new JobConf(Extract.class);
-                
                 conf.setJobName("ExtractInfo");
 	        conf.setMapperClass(Extract.Map.class);
 	        conf.setReducerClass(Extract.Reduce.class);
@@ -28,14 +31,11 @@ public class PageRank extends Configured implements Tool {
 	        conf.setOutputFormat(TextOutputFormat.class);
 	        conf.set("xmlinput.start","<page>");
 	        conf.set("xmlinput.end","</page>");
-
-	        FileInputFormat.setInputPaths(conf, inpath);
-        	FileOutputFormat.setOutputPath(conf, outpath);
-        	
+	        FileInputFormat.setInputPaths(conf, inPath);
+        	FileOutputFormat.setOutputPath(conf, graphPath);
         	JobClient.runJob(conf);
 
-                conf = new JobConf(CountPages.class);
-                
+                conf = new JobConf(CountPages.class);                
                 conf.setJobName("CountPages");
 	        conf.setMapperClass(CountPages.Map.class);
 	        conf.setReducerClass(CountPages.Reduce.class);
@@ -43,13 +43,51 @@ public class PageRank extends Configured implements Tool {
 	        conf.setOutputValueClass(Text.class);
 	        conf.setInputFormat(TextInputFormat.class);
 	        conf.setOutputFormat(TextOutputFormat.class);
-
-	        FileInputFormat.setInputPaths(conf, outpath);
-	        Path out2 = new Path(args[1] + "Count");
-        	FileOutputFormat.setOutputPath(conf, out2);
-        	
+	        FileInputFormat.setInputPaths(conf, graphPath);
+        	FileOutputFormat.setOutputPath(conf, countPath);
         	JobClient.runJob(conf);
-        			
+        	
+        	//JobConf conf = new JobConf(Rank.class);
+                FileSystem fs;// = FileSystem.get(conf);
+                //Path inpath = new Path(" ");
+                //Path outpath = new Path(" ");
+                int numIter = 8;
+                
+                for(int i = 1; i<=numIter; i++){
+                        iterInPath = (i == 1) ? graphPath : (new Path("outputOfThe"+String.valueOf(i-1)));
+                        iterOutPath = new Path("outputOfThe"+String.valueOf(i));
+                        conf = new JobConf(Rank.class);
+                  
+                        conf.setJobName("Rank Iterations");
+		        conf.setMapperClass(Rank.Map.class);
+		        conf.setReducerClass(Rank.Reduce.class);
+		        conf.setOutputKeyClass(Text.class);
+		        conf.setOutputValueClass(Text.class);
+		        conf.setInputFormat(TextInputFormat.class);
+		        conf.setOutputFormat(TextOutputFormat.class);
+		        FileInputFormat.setInputPaths(conf, iterInPath);
+	        	FileOutputFormat.setOutputPath(conf, iterOutPath);
+	        	JobClient.runJob(conf);
+	        	fs = FileSystem.get(conf);
+	        	if(i!=1) {
+	        	        fs.delete(new Path("outputOfThe"+String.valueOf(i-1)),true);
+	        	}
+                }
+                
+                //conf = new JobConf(SortRank.class);
+                //conf.setJobName("SortRank");
+		//conf.setMapperClass(SortRank.Map.class);
+		//conf.setReducerClass(SortRank.Reduce.class);
+		//conf.setOutputKeyClass(Text.class);
+		//conf.setOutputValueClass(Text.class);
+		//conf.setInputFormat(TextInputFormat.class);
+		//conf.setOutputFormat(TextOutputFormat.class);
+		//FileInputFormat.setInputPaths(conf, new Path("outputOfThe"+String.valueOf(numIter)));
+	        //FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+	        //JobClient.runJob(conf);
+	        //fs = FileSystem.get(conf);
+	        //fs.delete(new Path("outputOfThe"+String.valueOf(numIter)),true);
+	        		
 		return 0;
 	}
 
---
+++ Rank.java
@@ -0,0 +1,123 @@
+package PageRank;
+
+import java.io.*;
+import java.util.*;
+
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.filecache.DistributedCache;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.util.*;
+
+public class Rank extends Configured{
+
+	public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text> {
+
+		static enum Counters {INPUT_WORDS}
+
+		private Text outputKey = new Text();
+		private Text outputValue = new Text();
+		private Text thisLink = new Text();
+		private double rank = 0.0;
+		private int degree = 0;
+
+		public void map(LongWritable key, Text value, OutputCollector<Text, Text> output, Reporter reporter)
+		        throws IOException {
+				
+			String line = value.toString();
+			String[] parts = line.split("[ \t]");
+
+			thisLink.set(parts[0]);
+			if (parts.length >= 2) {
+				if (parts[1].length() < 10) { // A rank b c...
+					rank = Double.valueOf(parts[1]);
+					degree = parts.length - 2;
+
+					StringBuilder builder = new StringBuilder();
+					if (parts.length >= 3) {
+						for (int i = 2; i < parts.length; i++) {
+							builder.append(parts[i] + " ");
+						}
+					}
+					outputValue.set("info " + String.valueOf(degree) + " "
+							+ builder.toString());
+					output.collect(thisLink, outputValue);
+
+					outputValue.set(thisLink.toString() + " "
+							+ String.valueOf(rank) + " "
+							+ String.valueOf(degree));
+					if (parts.length >= 3) {
+						for (int i = 2; i < parts.length; i++) {
+							outputKey.set(parts[i]);
+							output.collect(outputKey, outputValue);
+							reporter.incrCounter(Counters.INPUT_WORDS, 1);
+						}
+					}
+				} else { // A (no rank) b c ...
+					rank = 1.0;
+					degree = parts.length - 1;
+
+					StringBuilder builder = new StringBuilder();
+					if (parts.length >= 2) {
+						for (int i = 1; i < parts.length; i++) {
+							builder.append(parts[i] + " ");
+						}
+					}
+					outputValue.set("info " + String.valueOf(degree) + " "
+							+ builder.toString());
+					output.collect(thisLink, outputValue);
+
+					outputValue.set(thisLink.toString() + " "
+							+ String.valueOf(rank) + " "
+							+ String.valueOf(degree));
+					for (int i = 1; i < parts.length; i++) {
+						outputKey.set(parts[i]);
+						output.collect(outputKey, outputValue);
+						reporter.incrCounter(Counters.INPUT_WORDS, 1);
+					}
+				}
+			} else {// only A
+				rank = 1.0;
+				degree = parts.length - 1;
+
+				outputValue.set("info 0");
+				output.collect(thisLink, outputValue);
+			}
+
+		}
+	}
+
+	public static class Reduce extends MapReduceBase implements Reducer<Text, Text, Text, Text> {
+
+		private double factor = 0.85;
+		private Text outputValue = new Text();
+		
+		public void reduce(Text key, Iterator<Text> values, OutputCollector<Text, Text> output, Reporter reporter)
+			throws IOException {
+
+			double sum = 0.00;
+			String outlinks = "";
+
+			while (values.hasNext()) {
+				String line = values.next().toString();
+				String[] parts = line.split("[ \t]");
+
+				if (!parts[0].equals("info")) {//Not info message
+					sum += Double.valueOf(parts[1]) / Double.valueOf(parts[2]);
+				} else {
+					if (parts.length > 2) {
+						for (int i = 2; i < parts.length; i++) {
+							outlinks = outlinks.concat(parts[i] + " ");
+						}
+					}
+				}
+			}
+			sum = factor * sum + 1 - factor;
+			sum = Math.round(sum * 10000.0) / 10000.0;
+			outputValue.set(String.valueOf(sum) + " " + outlinks);
+			output.collect(key, outputValue);
+		}
+	}
+
+}
